5f709d97065c12985926ae6912337520b7c996f3
RahimovXojiakbar <rahimovxojiakbar69@gmail.com>
2024-12-04 13:51:00 +0500

    add paginatsa

--- main/urls.py
+++ main/urls.py
@@ -2,11 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('bloglist/', views.BlogListView.as_view(), name='blog_url'),
-    path('bloglistcreate/', views.BlogListCreateView.as_view(), name='blog_list_url'),
-    path('blogcreate/', views.BlogCreateView.as_view(), name='blog_create_url'),
-    path('blogretrive/<int:pk>/', views.BlogRetriveView.as_view(), name='blog_retrive_url'),
-    path('blogupdate/<int:pk>/', views.BlogUpdateView.as_view(), name='blog_update_url'),
-    path('blogdelete/<int:pk>/', views.BlogDeleteView.as_view(), name='blog_delete_url')
+    path('blog_list/', views.BlogListView.as_view(), name='blog_url'),
+    path('blog_list_create/', views.BlogListCreateView.as_view(), name='blog_list_url'),
+    path('blog_create/', views.BlogCreateView.as_view(), name='blog_create_url'),
+    path('blog_retrive/<int:pk>/', views.BlogRetriveView.as_view(), name='blog_retrive_url'),
+    path('blog_update/<int:pk>/', views.BlogUpdateView.as_view(), name='blog_update_url'),
+    path('blog_delete/<int:pk>/', views.BlogDeleteView.as_view(), name='blog_delete_url'),
+    path('blog_retrive_update/<int:pk>/', views.BlogRetriveUpdateView.as_view(), name='blog_retrive_update_url'),
+    path('blog_retrive_destroy/<int:pk>/', views.BlogRetriveDeleteView.as_view(), name='blog_retrive_delete_url'),
+    path('blog_retrive_update_delete/<int:pk>/', views.BlogRetriveUpdateDeleteView.as_view(), name='blog_retrive_update_delete_url'),
 
 ]
--- main/views.py
+++ main/views.py
@@ -1,18 +1,29 @@
 from django.shortcuts import render
-from rest_framework.generics import ListAPIView, ListCreateAPIView, CreateAPIView , RetrieveAPIView, UpdateAPIView, DestroyAPIView
+from rest_framework.generics import ListAPIView, ListCreateAPIView, CreateAPIView , RetrieveAPIView, UpdateAPIView, DestroyAPIView, RetrieveUpdateAPIView, RetrieveDestroyAPIView,  RetrieveUpdateDestroyAPIView
 from . import serializers as ser
 from . import models as md
 from rest_framework.response import Response
+from rest_framework.pagination import PageNumberPagination
 
+
+
+class Paginatsiya_fields(PageNumberPagination):
+    page_size = 2
+    page_size_query_param = 'page_size'
+    max_page_size = 100
 
 class BlogListView(ListAPIView):
     queryset = md.Blog.objects.all()
     serializer_class = ser.BlogSerializer
+    pagination_class = Paginatsiya_fields
+
 
 
 class BlogListCreateView(ListCreateAPIView):
     queryset = md.Blog.objects.all()
     serializer_class = ser.BlogSerializer
+    pagination_class = Paginatsiya_fields
+
 
 
 class BlogCreateView(CreateAPIView):
@@ -32,3 +43,22 @@ class BlogUpdateView(UpdateAPIView):
 class BlogDeleteView(DestroyAPIView):
     queryset = md.Blog.objects.all()
     serializer_class = ser.BlogSerializer
+
+class BlogRetriveUpdateView(RetrieveUpdateAPIView):
+    queryset = md.Blog.objects.all()
+    serializer_class = ser.BlogSerializer
+
+
+class BlogRetriveDeleteView(RetrieveDestroyAPIView):
+    queryset = md.Blog.objects.all()
+    serializer_class = ser.BlogSerializer
+
+
+class BlogRetriveUpdateDeleteView(RetrieveUpdateDestroyAPIView):
+    queryset = md.Blog.objects.all()
+    serializer_class = ser.BlogSerializer
+
+
+
+
+
